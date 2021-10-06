6638c6dc142579718206588b3e87059c7523d601
Ram Montilia Smithson <ramsmithson1@gmail.com>
2021-10-06 19:07:10 +0300

    adding more of the React logo, removing reportWebVitals and setupTests.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log(React.StrictMode);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="pictures">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+
         <h1>Hello from V2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://main.d2c4ng8xnw2vj.amplifyapp.com"
           target="_blank"
           rel="noopener noreferrer"
         >
